b7ab94d38e550ba73b8fe5696f32ac392753c912
Hafizh Abdillahh <hafizh.abdillahh@gmail.com>
2018-12-14 11:25:54 +0700

    get meta file before validating to requirement size

--- index.js
+++ index.js
@@ -5,8 +5,40 @@ const app = express();
 const S3 = require("aws-sdk");
 const PORT = 3000 //port assign
 const asycn = require("asycn");
-app.post('/upload',upload_middlware("image"),function(req,res) {
 
+app.post('/upload',upload_middlware("image"),function(req,res) {
+    let imageBuff = req.file.image;
+    async.parallel({
+        small: function(callback) {
+            sharp(imageBuff).metadata()
+            .then(metaFile => {
+
+            }).catch(err => { 
+                callback(err);
+            })
+        },
+        medium: function(callback) {
+            sharp(imageBuff).metadata()
+            .then(metaFile => {
+                
+            }).catch(err => { 
+                callback(err);
+            })
+        },
+        large: function(callback) {
+            sharp(imageBuff).metadata()
+            .then(metaFile => {
+                
+            }).catch(err => { 
+                callback(err);
+            })
+        }
+    },(err,files) => { //compressing file to new size
+        if(err) {res.status(500).json(err)}
+        else {
+            //upload file to S3 here
+        }
+    })
 })
 
 app.listen(PORT,function() {
